a2d3bc164d066cec7640a54f8bcb2de128c816fd
Cakra Wangsa M.A.W <ckrawangsa@gmail.com>
2025-02-17 22:44:24 +0700

    Bismillah Jobsheet 2 v2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return 'NIM: 2341720032 <br> Nama: Cakra Wangsa M.A.W';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -59,3 +59,23 @@ Route::get('/routeProfile', function () {
 
 Route::view('/welcome', 'welcome');
 Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
